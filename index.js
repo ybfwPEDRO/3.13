b2c4ef2d07bfadd577d63d600a2583755b907f7d
dr7o <dr7o@fresnoU.org>
2022-05-10 13:32:03 -0700

    commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const cardList = document.querySelector( '.cardList')
+
+addCard('test');
+
+function addCard(value){
+    let card= document.createElement('div');
+    card.classList.add('card');
+    card.classList.add('active');
+    card.innerHTML = value;
+    cardList.appendChild(card);
+}
+
+function buildBoard(){
+    for (let i=0; i<12; i++){
+        addCard('starter');
+    }
+}
